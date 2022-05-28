84cec73981b0447dc4b6f4f7490ae78b5caaec27
Julio Neto <nettojulio@hotmail.com>
2022-05-28 11:01:25 -0300

    main class

--- src/main/java/br/com/julioneto/desafiocrud/DesafioSemana6desafioSemana6IlabApplication.java
+++ src/main/java/br/com/julioneto/desafiocrud/DesafioSemana6desafioSemana6IlabApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DesafioSemana6desafioSemana6IlabApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DesafioSemana6desafioSemana6IlabApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioSemana6desafioSemana6IlabApplication.class, args);
+    }
 
 }
