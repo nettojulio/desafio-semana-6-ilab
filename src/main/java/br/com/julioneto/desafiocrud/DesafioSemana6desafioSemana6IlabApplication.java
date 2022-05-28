dbe538c5d9bb7abb15af590bc35cb331fc3a37b4
Julio Neto <nettojulio@hotmail.com>
2022-05-27 23:17:46 -0300

    initial commit

---
+++ src/main/java/br/com/julioneto/desafiocrud/DesafioSemana6desafioSemana6IlabApplication.java
@@ -0,0 +1,13 @@
+package br.com.julioneto.desafiocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSemana6desafioSemana6IlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSemana6desafioSemana6IlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/julioneto/desafiocrud/DesafioSemana6desafioSemana6IlabApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.julioneto.desafiocrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSemana6desafioSemana6IlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
